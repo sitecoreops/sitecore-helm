3d53584d433aa53e834971feee757e9f1e3ff062
Per Manniche Bering <per.bering@valtech.com>
2020-04-15 20:48:04 +0200

    draft

---
+++ Build.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = "STOP"
+
+$repoPath = (Join-Path $PSScriptRoot ".\repo")
+
+# lint
+helm lint (Join-Path $PSScriptRoot ".\charts\sitecore-xm") --values (Join-Path $PSScriptRoot ".\charts\sitecore-xm\ci\values.yaml") --strict
+
+$LASTEXITCODE -ne 0 | Where-Object { $_ } | ForEach-Object { throw "Failed." }
+
+# package
+helm package (Join-Path $PSScriptRoot ".\charts\sitecore-xm") --version 1.0.0 --destination $repoPath
+
+$LASTEXITCODE -ne 0 | Where-Object { $_ } | ForEach-Object { throw "Failed." }
+
+# update index
+helm repo index $repoPath --merge (Join-Path $repoPath "\index.yaml")
+
+$LASTEXITCODE -ne 0 | Where-Object { $_ } | ForEach-Object { throw "Failed." }
---
+++ README.md
@@ -0,0 +1,8 @@
+# Sitecore Helm charts
+
+...
+
+## TODO's
+
+- Document values: [https://helm.sh/docs/chart_best_practices/values/#document-valuesyaml](https://helm.sh/docs/chart_best_practices/values/#document-valuesyaml)
+- Set `required` on required values.
